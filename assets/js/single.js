16c7a4e130c416a1ec0a69e07da6c1fe8f663b12
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-01-07 01:46:36 -0600

    made the commands dynamic based on user input, connected the two sheets in js, and added issue handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        // run the fetch request with the repo name
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     // format the github api url
@@ -20,7 +38,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -78,4 +97,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
